448a6f0cba05a81f468d28895a76e46aab97ac6d
Matt <mattgottlieb96@gmail.com>
2020-09-13 22:28:04 -0700

    transferring over

--- displaytest.py
+++ displaytest.py
@@ -5,8 +5,7 @@ import time
 if __name__=="__main__":
     twit=Twitter()
     coffee_choice,amount_votes=twit.getVote()
-    #coffee_choice="mediumroast"
-    #amount_votes=1
+    #coffee_choice,amount_votes="mediumroast",1
     brew=BrewCoffee(coffee_choice)
     print(coffee_choice,amount_votes)
     brew.displayCoffeeChoice(coffee_choice,amount_votes)
